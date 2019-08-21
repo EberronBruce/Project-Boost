84845f3ade9acc669675d8f23956d4260aa190c9
EberronBruce <bruce@redravencomputing.com>
2019-08-20 21:07:05 -0600

    added rocket

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
 	Rigidbody rigidbody;
+	AudioSource audioSource;
 	// Start is called before the first frame update
 	void Start() {
 		rigidbody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
@@ -20,6 +22,11 @@ public class Rocket : MonoBehaviour {
 		if(Input.GetKey(KeyCode.Space)) { // can thrust while rotating
 
 			rigidbody.AddRelativeForce(Vector3.up);
+			if(!audioSource.isPlaying) {
+				audioSource.Play();
+			}
+		} else {
+			audioSource.Stop();
 		}
 
 		if(Input.GetKey(KeyCode.A)) {
