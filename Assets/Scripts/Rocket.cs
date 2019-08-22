b5e63ff9956e1fe9fadc0621a3ab4e6d8ef2c9cd
EberronBruce <bruce@redravencomputing.com>
2019-08-21 19:55:29 -0600

    Added better controls

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
+	bool soundPlaying = true;
+	float startVolume = 1f;
+
+	[SerializeField] float rcsThrust = 100f;
+	[SerializeField] float mainThrust = 100f;
 
 	Rigidbody rigidbody;
 	AudioSource audioSource;
@@ -15,24 +20,58 @@ public class Rocket : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-		ProcessInput();
+		Thrust();
+		Rotate();
 	}
 
-	private void ProcessInput() {
+	private void Thrust() {
+		
 		if(Input.GetKey(KeyCode.Space)) { // can thrust while rotating
-
-			rigidbody.AddRelativeForce(Vector3.up);
-			if(!audioSource.isPlaying) {
+			rigidbody.freezeRotation = true; // take manual control of rotation
+			rigidbody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+			if(!soundPlaying) {
+				soundPlaying = true;
+				audioSource.volume = startVolume;
 				audioSource.Play();
 			}
 		} else {
-			audioSource.Stop();
-		}
-
-		if(Input.GetKey(KeyCode.A)) {
-			transform.Rotate(Vector3.forward);
-		} else if(Input.GetKey(KeyCode.D)) {
-			transform.Rotate(-Vector3.forward);
+			rigidbody.freezeRotation = false;  //resume physics control of rotation
+			if(soundPlaying) {
+				soundPlaying = false;
+				StartCoroutine(VolumeFade(audioSource, 0f, 0.5f));
+			}
 		}
 	}
+
+	private void Rotate() {
+		rigidbody.freezeRotation = true; // take manual control of rotation
+
+		float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+		if(Input.GetKey(KeyCode.A)) {
+			transform.Rotate(Vector3.forward * rotationThisFrame);
+		} else if(Input.GetKey(KeyCode.D)) {
+			transform.Rotate(-Vector3.forward * rotationThisFrame);
+		}
+
+		rigidbody.freezeRotation = false;  //resume physics control of rotation
+	}
+
+	
+	IEnumerator VolumeFade(AudioSource _AudioSource, float _EndVolume, float _FadeLength) {
+		float _StartTime = Time.time;
+		while(!soundPlaying && Time.time < _StartTime + _FadeLength) {
+			float alpha = (_StartTime + _FadeLength - Time.time) / _FadeLength;
+			// use the square here so that we fade faster and without popping
+			alpha = alpha * alpha;
+			_AudioSource.volume = alpha * startVolume + _EndVolume * (1.0f - alpha);
+
+			yield return null;
+
+		}
+
+		if(_EndVolume < 0.1) {
+			_AudioSource.UnPause();
+		}
+	}// end VolumeFade()
 }
